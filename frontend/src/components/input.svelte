ec314d1ea799589e22a7beff7750eef9452c082a
Alexander Schau <alexander@schau.email>
2021-09-30 18:35:28 +0200

    add main page with form

---
+++ frontend/src/components/input.svelte
@@ -0,0 +1,36 @@
+<script>
+  const genID = (len) => {
+    let result = "";
+    let characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (var i = 0; i < len; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  };
+  let eventName = genID(20);
+</script>
+
+<div class="flex flex-wrap my-4 px-2 bg-gray-100 w-full rounded-lg">
+  <input
+    type="text"
+    bind:value={eventName}
+    placeholder="my-event"
+    class="bg-transparent flex-1 my-2 px-4 py-1 outline-none overflow-ellipsis"
+  />
+  <div
+    on:click={() => (eventName = genID(20))}
+    class="bg-white hover:bg-gray-100 px-2 py-1 my-2 rounded-lg border-black border cursor-pointer whitespace-nowrap flex-grow"
+  >
+    random event
+  </div>
+</div>
+<div class="flex">
+  <a target="_blank" href={`/receive/${eventName}`} class="button mx-2 flex-1">
+    await call
+  </a>
+  <a target="_blank" href={`/send/${eventName}`} class="button mx-2 flex-1">
+    send trigger
+  </a>
+</div>
